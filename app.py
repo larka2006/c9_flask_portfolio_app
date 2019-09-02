dc8026434a21715c351c24b3d88dc6ad59916cd0
Larisa Golovko <30803793+larka2006@users.noreply.github.com>
2019-09-01 20:24:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,29 +35,7 @@ def add_numbers_post():
   	      except ValueError:
   	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
 
-@app.route('/guess_number', methods=['GET','POST'])
-def guess_number_post():
-	if request.method == 'GET':
-	  	return render_template('guess_number.html')
-	elif request.method == 'POST':
-  	     secretNumber = random.randint(1, 30)
-             print('I am thinking of a number between 1 and 30.')
-	     print(request.form['text'])
-# Ask the player to guess 6 times.
-         for guessesTaken in range(1, 7):
-	    print('Take a guess.')
-	    guess = int(input())
-	    if guess < secretNumber:
-	        print('Your guess is too low.')
-    	    elif guess > secretNumber:
-		print('Your guess is too high.')
-            else:
-                break    # This condition is the correct guess!
-            if guess == secretNumber:
-                 print('Good job! You guessed my number in ' + str(guessesTaken) + ' guesses!')
-            else:
-                 print('Nope. The number I was thinking of was ' + str(secretNumber))
-	 
+
   	      
 @app.route('/shopping_list', methods=['GET','POST'])
 def shopping_list_post():
